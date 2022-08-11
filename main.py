e1e902b81e091e6492d26904bd0b304470f77867
Vinícius Lazzari <vinilazzari028@gmail.com>
2022-08-10 22:53:58 -0300

    ✨ Add moving average score

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from scores.moving_average import return_moving_average_score
+from scores.put_and_call import return_put_and_call_score
+
 def main():
-    print("Hello world!")
+    moving_average_score = return_moving_average_score()
+    put_and_call_score = return_put_and_call_score()
+
+    print(moving_average_score, put_and_call_score)
     
 if __name__ == "__main__":
     main()
---
+++ scores/moving_average.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+ATTRIBUTE_WEIGHT = 25
+MOVING_AVERAGE_VALUE = 100
+
+def return_moving_average_score():
+    ibov = pd.read_csv("data/IBOV.csv", index_col = 'Date', parse_dates = True)
+    
+    ibov['MA'] = ibov['Close'].rolling(MOVING_AVERAGE_VALUE).mean()
+
+    list_of_moving_averages = ibov.tail(MOVING_AVERAGE_VALUE)["MA"].to_list()
+
+    today_value = list_of_moving_averages.pop()
+
+    position = 0
+
+    for item in list_of_moving_averages:
+        if item < today_value:
+            position += 1
+
+    score = (ATTRIBUTE_WEIGHT / len(list_of_moving_averages)) * position
+
+    return score
